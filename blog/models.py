66c26e67d6dc2cc1778d0470699b3fb8a49816da
MD ARMAAN ANSARI <103968837+md-armaan13@users.noreply.github.com>
2023-06-12 02:26:58 +0530

    addind models using sqllite, migrate data,in gui

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,19 @@
 from django.db import models
-
+from django.utils import timezone
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class Post(models.Model):
+    # max length of the title is 100 characters
+    title = models.CharField(max_length=100)
+    content = models.TextField()  # no limit on the length of the text
+    # auto_now = True means that the date will be updated automatically
+    date_posted = models.DateTimeField(default=timezone.now)
+    # when the post is updated
+    # use auto_now_add = True if you want the date to be updated only when the post is created
+    # default = timezone.now can also be used
+    # if the user is deleted, the post will also be deleted
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    # if you want to keep the post even after the user is deleted, use on_delete = models.SET_NULL
+    # this is one to many relationship, one user can have many posts, but one post can have only one user
--- blog/views.py
+++ blog/views.py
@@ -1,29 +1,18 @@
 from django.shortcuts import render
 
-from django.http import HttpResponse # sending the response
-
-posts = [
-    {
-        'author' : 'CoreyMS',
-        'title' : 'Blog Post 1',
-        'content' : 'First post content',
-        'date_posted' : 'August 27, 2018'
-    },
-    {
-        'author' : 'Jane Doe',
-        'title' : 'Blog Post 2',
-        'content' : 'Second post content',
-        'date_posted' : 'August 28, 2018'
-    }
-]
+from django.http import HttpResponse  # sending the response
+from .models import Post
 
 
-def home (request) :
+def home(request):
+    posts = Post.objects.all()
     context = {
-        'posts' : posts
+        'posts': posts
     }
-    return render(request,'blog/home.html',context) #the Path for templates would be blog/templateName
+    # the Path for templates would be blog/templateName
+    return render(request, 'blog/home.html', context)
 
-def about (request) :
-    return render(request,'blog/about.html')
+
+def about(request):
+    return render(request, 'blog/about.html')
 # Create your views here.
